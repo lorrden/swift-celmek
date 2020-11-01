c6c096248eb4628301165f078fa014dffc59967a
Mattias Holm <lorrden@openorbit.org>
2020-11-01 19:31:42 +0100

    Port over ELP2000 algorithm (#2)

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ The celmek library implements several astronomical algorithms.
 | Algorithm | Status
 | --- | --- 
 | VSOP87 | Done
+| ELP2000 82b | Done
 
 ## License
 
---
+++ Tests/swift-celmekTests/elp2000-82b_tests.swift
@@ -0,0 +1,39 @@
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2020 Mattias Holm
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import XCTest
+@testable import swift_celmek;
+
+final class elp2000_82b_tests : XCTestCase {
+  func testELP2000() {
+
+    let res = cm_elp2000_82b(2448724.5);
+    
+    XCTAssertEqual(res.x, cm_degToRad(133.162659), accuracy: 0.00000002)
+    XCTAssertEqual(res.y, cm_degToRad(-3.229127), accuracy: 0.000000005)
+    XCTAssertEqual(res.z, 368409.7e3, accuracy: 100.0)
+  }
+
+  static var allTests = [
+    ("testELP2000", testELP2000),
+  ]
+}
+
+
+
+
