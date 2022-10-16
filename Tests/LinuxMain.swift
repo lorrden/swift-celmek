363f0540924f843a07a7a22069ad2a467d260336
Mattias Holm <lorrden@openorbit.org>
2022-10-16 08:41:48 +0200

    Update swift version to 5.6

--- Tests/LinuxMain.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-
-import swift_celmekTests
-
-var tests = [XCTestCaseEntry]()
-tests += swift_celmekTests.__allTests()
-
-XCTMain(tests)
--- Tests/swift-celmekTests/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Mattias Holm on 2020-11-01.
-//
-
-import Foundation
