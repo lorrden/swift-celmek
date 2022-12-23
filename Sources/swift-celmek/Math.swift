8bee0bb4a9c61cd9df0bec1d491e93b3eea336f3
Mattias Holm <lorrden@openorbit.org>
2022-12-23 10:03:41 +0100

    Add functions to work with angles and geo coords

--- Sources/swift-celmek/Math.swift
+++ Sources/swift-celmek/Math.swift
@@ -23,3 +23,65 @@ func cm_degToRad(_ deg: Double) -> Double {
 func cm_radToDeg(_ rad: Double) -> Double {
   return rad * 180.0 / .pi
 }
+
+extension Double {
+  func toRad() -> Double {
+    return self * (.pi / 180.0)
+  }
+  func toDeg() -> Double {
+    return self * (180.0 / .pi)
+  }
+  func degToHours() -> Double {
+    return self / 15.0
+  }
+  func hoursToDeg() -> Double {
+    return self * 15.0
+  }
+}
+
+struct RightAscension {
+  var hours: Double
+  var minutes: Double
+  var seconds: Double
+}
+extension RightAscension {
+  func toDeg() -> Double {
+    let totalHours = self.hours.magnitude + self.minutes / MINUTES_PER_HOUR + self.seconds / SECONDS_PER_HOUR
+
+    if self.hours.sign == .plus {
+      return totalHours.hoursToDeg()
+    } else {
+      return -totalHours.hoursToDeg()
+    }
+  }
+  
+  func toRad() -> Double {
+    return toDeg().toRad()
+  }
+}
+
+@frozen
+public struct GeographicCoordinate {
+  var latitude: Double
+  var longitude: Double
+}
+
+struct AngleOfArc {
+  var degrees: Double
+  var minutes: Double
+  var seconds: Double
+}
+extension AngleOfArc {
+  func toDeg() -> Double {
+    let totalDegrees = self.degrees.magnitude + self.minutes / MINUTES_OF_ARC_PER_DEGREE + self.seconds / SECONDS_OF_ARC_PER_DEGREE
+    if self.degrees.sign == .plus {
+      return totalDegrees
+    } else {
+      return -totalDegrees
+    }
+  }
+  
+  func toRad() -> Double {
+    return toDeg().toRad()
+  }
+}
---
+++ Tests/swift-celmekTests/math_tests.swift
@@ -0,0 +1,54 @@
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2020 Mattias Holm
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import XCTest
+@testable import swift_celmek;
+
+final class math_tests : XCTestCase {
+  func testRadToDeg() {
+    XCTAssertEqual(0.0.toDeg(), 0.0);
+    XCTAssertEqual(1.0.toDeg(), 57.2958, accuracy:  0.00005);
+  }
+  
+  func testDegToRad() {
+    XCTAssertEqual(0.0.toRad(), 0.0);
+    XCTAssertEqual(1.0.toRad(), 0.0174533, accuracy:  0.00000005);
+  }
+  
+  func testDegToHours() {
+    XCTAssertEqual(0.0.degToHours(), 0.0);
+    XCTAssertEqual(15.0.degToHours(), 1.0);
+  }
+  func testHoursToDeg() {
+    XCTAssertEqual(0.0.hoursToDeg(), 0.0);
+    XCTAssertEqual(1.0.hoursToDeg(), 15.0);
+  }
+  
+  func testRightAscensionToDeg() {
+    // Meeus, Astronomical Algorithms 2nd edition, Example 1.a
+    let ra = RightAscension(hours: 9, minutes: 14, seconds: 55.8)
+    XCTAssertEqual(ra.toDeg(), 138.73250, accuracy: 0.000005);
+  }
+  
+  func testRightAscensionToRad() {
+    let ra = RightAscension(hours: 9, minutes: 14, seconds: 55.8)
+    XCTAssertEqual(ra.toRad(), 2.4213389045, accuracy: 1.0e-6);
+  }
+}
+
+
