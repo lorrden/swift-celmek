8db18cb831394ca930166604b7d30ff42d708ff5
Mattias Holm <lorrden@openorbit.org>
2022-12-23 09:59:05 +0100

    Add functions to calculate the day of easter

---
+++ Sources/swift-celmek/DayOfEaster.swift
@@ -0,0 +1,54 @@
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2020 Mattias Holm
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+func gregorianDateOfEaster(year: Int) -> GregorianDate {
+  let a = year % 19
+  let b = year / 100
+  let c = year % 100
+  let d = b / 4
+  let e = b % 4
+  let f = (b + 8) / 25
+  let g = (b - f + 1) / 3
+  let h = (19*a + b - d - g + 15) % 30
+  let i = c / 4
+  let k = c % 4
+  let l = (32 + 2 * e + 2 * i - h - k) % 7
+  let m = (a + 11 * h + 22 * l) / 451
+  let n = (h + l - 7 * m + 114) / 31
+  let p = (h + l - 7 * m + 114) % 31
+  
+  return GregorianDate(year: year,
+                       month: Month(rawValue: Int32(n))!,
+                       day: Double(p+1))
+}
+
+func julianDateOfEaster(year: Int) -> JulianDate {
+  let a = year % 4
+  let b = year % 7
+  let c = year % 19
+  let d = (19 * c + 15) % 30
+  let e = (2 * a + 4 * b - d + 34) % 7
+  let f = (d + e + 114) / 31
+  let g = (d + e + 114) % 31
+  
+  return JulianDate(year: year,
+                    month: Month(rawValue: Int32(f))!,
+                    day: Double(g+1))
+}
---
+++ Tests/swift-celmekTests/DayOfEaster_tests.swift
@@ -0,0 +1,73 @@
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2020 Mattias Holm
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import XCTest
+@testable import swift_celmek;
+
+final class dayOfEaster_tests : XCTestCase {
+  func testGregorianDateOfEaster() {
+    // Meeus, p68
+    let date_0 = gregorianDateOfEaster(year: 1991)
+    XCTAssertEqual(date_0.year, 1991)
+    XCTAssertEqual(date_0.month, .March)
+    XCTAssertEqual(date_0.day, 31)
+
+    let date_1 = gregorianDateOfEaster(year: 1992)
+    XCTAssertEqual(date_1.year, 1992)
+    XCTAssertEqual(date_1.month, .April)
+    XCTAssertEqual(date_1.day, 19)
+
+    let date_2 = gregorianDateOfEaster(year: 1993)
+    XCTAssertEqual(date_2.year, 1993)
+    XCTAssertEqual(date_2.month, .April)
+    XCTAssertEqual(date_2.day, 11)
+
+    let date_3 = gregorianDateOfEaster(year: 1954)
+    XCTAssertEqual(date_3.year, 1954)
+    XCTAssertEqual(date_3.month, .April)
+    XCTAssertEqual(date_3.day, 18)
+
+    let date_4 = gregorianDateOfEaster(year: 2000)
+    XCTAssertEqual(date_4.year, 2000)
+    XCTAssertEqual(date_4.month, .April)
+    XCTAssertEqual(date_4.day, 23)
+
+    let date_5 = gregorianDateOfEaster(year: 1818)
+    XCTAssertEqual(date_5.year, 1818)
+    XCTAssertEqual(date_5.month, .March)
+    XCTAssertEqual(date_5.day, 22)
+  }
+  
+  func testJulianDateOfEaster() {
+    let date_0 = julianDateOfEaster(year: 179)
+    XCTAssertEqual(date_0.year, 179)
+    XCTAssertEqual(date_0.month, .April)
+    XCTAssertEqual(date_0.day, 12)
+
+    let date_1 = julianDateOfEaster(year: 711)
+    XCTAssertEqual(date_1.year, 711)
+    XCTAssertEqual(date_1.month, .April)
+    XCTAssertEqual(date_1.day, 12)
+
+    let date_2 = julianDateOfEaster(year: 1243)
+    XCTAssertEqual(date_2.year, 1243)
+    XCTAssertEqual(date_2.month, .April)
+    XCTAssertEqual(date_2.day, 12)
+  }
+}
+
