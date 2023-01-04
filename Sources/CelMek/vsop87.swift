7b4be2eb8ed5cbaeae4bdf3d67ff58697bab378e
Mattias Holm <lorrden@openorbit.org>
2023-01-04 07:36:33 +0100

    Update copyright headers

--- Sources/CelMek/vsop87.swift
+++ Sources/CelMek/vsop87.swift
@@ -1,7 +1,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
-// Copyright 2020 Mattias Holm
+// Copyright 2020-2022 Mattias Holm
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
