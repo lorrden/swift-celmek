1256c20f117af7b1e7faeeb2c8da1aa0ead5e0fc
Mattias Holm <lorrden@openorbit.org>
2023-11-05 11:21:38 +0100

    Cleanup

--- Tests/AerodynamicsTests/AirfoilTest.swift
+++ Tests/AerodynamicsTests/AirfoilTest.swift
@@ -9,16 +9,7 @@ import XCTest
 @testable import Aerodynamics
 
 final class AirfoilTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    func testParametricAirfoil() throws {
       let airfoilString = """
 name: NACA 2412
 generated-with: xfoil
